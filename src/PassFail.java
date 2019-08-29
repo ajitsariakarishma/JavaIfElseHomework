05ab7b2b6e73e8bc7097016e61bc1aa3c219ec32
ajitsariakarishma <karishmaajitsaria@gmail.com>
2019-08-29 19:04:40 +0100

    second commit

--- src/PassFail.java
+++ src/PassFail.java
@@ -4,31 +4,43 @@
 import java.util.Scanner;
 
 public class PassFail {
-    public static void result() {                                                   // method defined
+
+    public static double[] getDoubleArray() {                                                   // method defined
         Scanner scanner = new Scanner(System.in);      // creating object of Scanner package to get user input
         System.out.print("Enter student name :");
         String name = scanner.next();
         System.out.print("Enter Rollno. :");
         int rollno = scanner.nextInt();
+        double marks[] = new double[5];
         System.out.print(" Enter marks for English :");
-        double english = scanner.nextDouble();
+        marks[0] = scanner.nextDouble();
         System.out.print(" Enter marks for Maths :");
-        double maths = scanner.nextDouble();
+        marks[1] = scanner.nextDouble();
         System.out.print(" Enter marks for Science :");
-        double science = scanner.nextDouble();
-        if (english >= 35 && maths >= 35 && science >= 35) {      //defining condition for result
+        marks[2] = scanner.nextDouble();
+         marks[3] = marks[0]+marks[1]+marks[2];
+         marks[4]=(marks[3]*100)/300;
+        //return total;
+        return marks;
+
+    }
+
+    public static void main(String[] args) {
+
+        double a[] = getDoubleArray();
+
+        System.out.println("total marks objained : "+a[3]);
+        System.out.println("percentage obtaines is : "+a[4]+"%");
+        if (a[0] >= 35 && a[1] >= 35 && a[2] >= 35) {      //defining condition for result
             System.out.println("Result : Pass");
         } else {
             System.out.println("Result : Fail");
         }
+
     }
-
-        public static void main (String[]args){
-
-            result();
-        }
-
 }
+
+
 
 
 
