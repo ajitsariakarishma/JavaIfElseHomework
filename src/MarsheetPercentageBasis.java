c89acebab4180b4447850f7f4a61a0608f668e85
ajitsariakarishma <karishmaajitsaria@gmail.com>
2019-08-29 19:04:18 +0100

    second commit

--- src/MarsheetPercentageBasis.java
+++ src/MarsheetPercentageBasis.java
@@ -3,47 +3,34 @@ import java.util.Scanner;
 grade according to the same
  */
 
-public class MarsheetPercentageBasis {                //Created a method
-    public static void studentdata() {
-        Scanner scanner = new Scanner(System.in);      //created object for Scanner Package
-        System.out.print("Enter student name :");
-        String name = scanner.next();
-        System.out.print("Enter Rollno. :");
-        int rollno = scanner.nextInt();
-        System.out.print("Enter marks for English :");
-        double english = scanner.nextDouble();
-        System.out.print("Enter marks for Maths :");
-        double maths = scanner.nextDouble();
-        System.out.print("Enter marks for Science :");
-        double science = scanner.nextDouble();
-        double total = english + maths + science;  // adding values entered by user to get total
-        System.out.println("Total marks obtained :" + total);
-        double percentage = total *100 / 300; //Calculating the percentage
+public class MarsheetPercentageBasis extends PassFail {
 
-        System.out.println("percentage :" + percentage+ "%");
-        if(percentage >=80){                             //grading according to percentage with if else ladder
-                    System.out.println("grade is A+");
-        } else if (percentage >= 60) {
+
+    public static void main(String[] args) {
+
+        double b[]=  getDoubleArray();
+        System.out.println("Total marks obtained : "+b[3]);
+        System.out.println("Percentage obtained is : "+b[4] +"%");
+        if (b[4] >= 80) {                   //grading according to percentage with if else ladder
+            System.out.println("grade is A+");
+        } else if (b[4]>= 60) {
             System.out.println("grade is A");
-        }else if(percentage>=50) {
+        } else if (b[4] >= 50) {
             System.out.println("grade is B");
-        }else if (percentage>=35){
+        } else if (b[4] >= 35) {
             System.out.println("grade is C");
-        }else{System.out.println("grade fail");
-
+        } else {
+            System.out.println("grade fail");
         }
-
-        }
-        public static void main (String[]args){
-
-            MarsheetPercentageBasis.studentdata();
-
-
-
-
-
-
-                   }
-
 
     }
+
+
+
+
+
+
+}
+
+
+
